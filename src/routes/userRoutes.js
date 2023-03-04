f2abd7176480404712a36f1f868316fe874ec11e
bakai_dmytro <hgkr63@motorolasolutions.com>
2023-03-04 20:49:28 +0200

    Updated jwt authorization, added method to generate Public&Private key.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const userController = require('../controllers/userController')
 const {loginSchema, signupSchema} = require("../validation/schema");
 const {validateRequest} = require("../validation/validateRequest");
+const authJwt = require('../security/authJwt');
 const {signUp, signIn, getUserById} = userController;
 
 
@@ -11,6 +12,6 @@ router.post('/signup',signupSchema, validateRequest, signUp)
 
 router.post('/login',loginSchema, validateRequest, signIn)
 
-router.get("/:userId", getUserById)
+router.get("/:userId", authJwt.verifyToken, authJwt.verifyUserId, getUserById)
 
 module.exports = router;
