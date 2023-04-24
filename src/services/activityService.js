a1f57c91dc166cb0d9afb217c27b2e1c9c9015cb
bakai_dmytro <hgkr63@motorolasolutions.com>
2023-04-24 16:09:25 +0300

    minor fixes

--- src/services/activityService.js
+++ src/services/activityService.js
@@ -2,31 +2,31 @@ const Activity = require('../../models').Activity;
 const Trip = require('../../models').Trip;
 
 const getActivityById = async (id) => {
-    return await Activity.findByPk(id, {attributes: ['id', 'name']});
+    return Activity.findByPk(id, {attributes: ['id', 'name']});
 }
 
 const getActivityByName = async (name) => {
-    return await Activity.findOne({where: {name: name}, attributes: ['id', 'name']});
+    return Activity.findOne({where: {name: name}, attributes: ['id', 'name']});
 }
 
 const getAllActivities = async () => {
-    return await Activity.findAll({attributes: ['id', 'name']});
+    return Activity.findAll({attributes: ['id', 'name']});
 }
 
 const createActivity = async (activity) => {
-    return await Activity.create(activity);
+    return Activity.create(activity);
 }
 
 const updateActivityById = async (id, activity) => {
-    return await Activity.update(activity, {where: {id}});
+    return Activity.update(activity, {where: {id}});
 }
 
 const deleteActivityById = async (id) => {
-    return await Activity.destroy({where: {id}});
+    return Activity.destroy({where: {id}});
 }
 
 const checkIfExists = async (field, value) => {
-    return await Activity.count({where: {[field]: value}}).then(count => {
+    return Activity.count({where: {[field]: value}}).then(count => {
             return count !== 0;
         }
     ).catch(err => {
